673badefb695ee6ef8281e02be582452ec086419
Simon9001 <Simongatungo300@gmail.com>
2025-02-19 01:02:53 -0800

    not so fast i see some things needs more work

--- core/tasks.py
+++
@@ -1,8 +0,0 @@
-from celery import shared_task
-from django.utils.timezone import now
-from core.models import MeetingHistory
-
-@shared_task
-def store_meeting_history(user_id, meeting_name):
-    MeetingHistory.objects.create(user_id=user_id, meeting_name=meeting_name, created_at=now())
-    return f"Meeting '{meeting_name}' stored for user {user_id}"
--- thebackend/settings.py
+++ thebackend/settings.py
@@ -142,7 +142,7 @@ CSRF_COOKIE_SECURE = False
 CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:8000",
-    "https://backendar.onrender.com"
+    "https://backendar.onrender.com",
     ]  # Ensure API calls are allowed
 
 # REST Framework Settings
